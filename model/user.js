72db6700d006866ddc98e9281ff7e1d070fba9bb
kankit08 <kankit08@gmail.com>
2022-09-20 20:20:56 +0530

    creating a user schema to get user details

--- model/user.js
+++ model/user.js
@@ -1,1 +1,31 @@
 // For register a user in database
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
