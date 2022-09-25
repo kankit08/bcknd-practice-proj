39f3d40d454868f64291e9e128eedf5550bc2e0a
kankit08 <kankit08@gmail.com>
2022-09-25 14:41:45 +0530

    Setting the token at multiple places

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,11 @@
 // For writing custom middlewares for user authentication
+
+const auth = require("jsonwebtoken");
+
+//Setting the token at multiple places
+const auth = (req, res, next) => {
+  const token =
+    req.cookies.token ||
+    req.header("Authorization").replace("Bearer ", "") ||
+    req.body.token;
+};
